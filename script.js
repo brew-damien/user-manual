15b1b46e93d9dd1f62659ad992c2c893cc27f327
DamienCook <dacook@brewdigital.uk>
2022-09-28 15:03:53 +0100

    added interactive nav bar

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-/* function myFunction() {
-    var element = document.getElementById("myDIV");
-    element.classList.toggle("mystyle");
-}
-*/
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
